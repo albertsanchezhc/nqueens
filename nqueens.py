6d988308ba962673b2b0420975a24298c5a1d5b8
albertsanchezhc <albertosanchez.hc@gmail.com>
2018-12-05 11:09:43 -0600

    calculates length

--- nqueens.py
+++ nqueens.py
@@ -1,4 +1,17 @@
 from itertool import permutations
 import sys
 
-n = check_equality(combo = None, clos=None, n_queens=None):
+n = int (sys.argv[1])
+
+def check_equality(combo = None, clos=None, n_queens=None):
+
+    #for each index i in the columns, finds the row value of that index and adds 1 to it
+    results_1 = list()
+    for i in cols:
+        res = combo[i]+i
+        results_1.append(res)
+
+    #calculates length of the unique values from the previous step
+    val1 = len(set(results_1))
+
+    
